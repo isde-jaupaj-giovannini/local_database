adaaca6494219d8f3a1744349bfc62358b0f31df
Davide Giovannini <giovannini.davide90@gmail.com>
2016-01-24 20:44:41 +0100

    Added missing annotation and fixed service url

--- src/com/unitn/local_database/LocalDBImpl.java
+++ src/com/unitn/local_database/LocalDBImpl.java
@@ -1,5 +1,6 @@
 package com.unitn.local_database;
 
+import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -8,12 +9,9 @@ import java.net.URISyntaxException;
 /**
  * Created by erinda on 1/24/16.
  */
-public class LocalDBImpl implements LocalDB{
-
-    @Override
-    public String getDescription() {
-        return LocalDB.class.getSimpleName();
-    }
+@WebService(endpointInterface = "com.unitn.local_database.LocalDB",
+        serviceName = "LocalDatabase")
+public class LocalDBImpl implements LocalDB {
 
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException {
         String PROTOCOL = "http://";
@@ -22,16 +20,21 @@ public class LocalDBImpl implements LocalDB{
             HOSTNAME = "localhost";
         }
         String PORT = "6904";
-        String BASE_URL = "/ws/storage";
+        String BASE_URL = "/ws/localdb";
 
         if (String.valueOf(System.getenv("PORT")) != "null") {
             PORT = String.valueOf(System.getenv("PORT"));
         }
 
         String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
-        System.out.println("Starting "+ LocalDB.class.getSimpleName() +"...");
+        System.out.println("Starting " + LocalDB.class.getSimpleName() + "...");
         System.out.println("--> Published. Check out " + endpointUrl + "?wsdl");
         Endpoint.publish(endpointUrl, new LocalDBImpl());
+    }
+
+    @Override
+    public String getDescription() {
+        return LocalDB.class.getSimpleName();
     }
 
 
